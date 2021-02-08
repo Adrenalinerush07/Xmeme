1df891ef29f67f0683b8970b920fc513d8a4e8bd
cyberghost002 <49005095+cyberghost002@users.noreply.github.com>
2021-02-09 00:24:21 +0530

    error handled and patch api

--- app.js
+++ app.js
@@ -11,35 +11,60 @@ app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(methodOverride('_method'))
 
+// Conecting to mongodb
 mongoose.connect('mongodb://localhost:27017/Xmeme', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
 
+// Route for home page (posting a new meme)
 app.get("/", function(req, res){
    res.render("post");
 });
 
+// Route for redirecting to edit page of a particular meme
+app.get('/update/:id', async (req, res) => {
+  const memes = await Post.findById(req.params.id)
+  res.status(200).render('edit', { memes })
+})
 
-app.post("/memes", async(req, res) => {  
+// Patch method for updating a meme
+// app.patch("updated/:id", function (res, res, next) {
+//   var id=req.params.id;
+//   var url = req.body.url;
+//   var caption = req.body.caption;
+//   console.log("kartik")
+//   Post.findById(id, function (err, data) {
+//     console.log(data)
+//     data.url=url;
+//     data.caption=caption;
+//     data.save(function (err) {
+//       if(err){
+//         throw err
+//       }
+//       res.send("Done")      
+//     })
+//   })
+// })
+
+app.patch('/updated/:id', async (req, res) => {
   try{
-    const name = req.body.name;
-    const url = req.body.url;
-    const caption = req.body.caption;
+    const memes = await Post.findById(req.params.id)
+    if(memes === null){
+      res.status(404).send("error")
+      return
+    }
+    memes.caption = req.body.caption;
+    memes.url = req.body.url;
 
-    const savePost = new Post({
-      name,
-      url,
-      caption
-    })
-    const saved = await savePost.save()
-    res.status(201).send(saved)
+    await memes.save();
+    res.status(201).redirect('/show'); 
   }
-  catch{
-    res.status(400).send({error: 'Unable to post'})
+  catch (err){
+    res.status(404).send("error",{error: err})
   }
+})
 
-});
-
+// Route for posting a meme
 app.post("/add", async(req, res) => {  
   try{
     const name = req.body.name;
@@ -54,33 +79,91 @@ app.post("/add", async(req, res) => {
     const saved = await savePost.save()
     res.status(201).redirect("show")
   }
-  catch{
-    res.status(400).send({error: 'Unable to post'})
+  catch (err){
+    res.status(404).send("error",{error: err})
   }
 
 });
 
-app.get('/show/:id', async (req, res) => {
-  console.log(req.params);
-  console.log('kartik');
-  const memes = await Post.findOne({ _id: req.params.id })
-  console.log(memes)
-  if (memes == null) res.redirect('/')
-  res.render('single', { memes: memes })
-})
+// ## Rendering meme
 
-
-app.get("/memes", async (req, res) => {
-  const memes = await Post.find().sort({createdAt: 'desc'})
-  res.status(200).send(memes)
-})
-
+// Route for showing all memes
 app.get("/show", async (req, res) => {
   const memes = await Post.find().sort({createdAt: 'desc'})
   res.status(200).render('results', {memes, error: undefined})
 })
 
+// Route for showing individual memes
+app.get('/show/:id', async (req, res) => {
+  const memes = await Post.findOne({ _id: req.params.id })
+  if(memes === null){
+    res.status(404).send("error");
+    return
+  }
+  res.status(200).render('single', { memes: memes })
+})
 
+
+// ## Routes set for checking
+
+// Getting all posted memes in chronological order
+app.get("/memes", async (req, res) => {
+  const memes = await Post.find().sort({createdAt: 'desc'})
+  res.status(200).send(memes)
+})
+
+app.get('/memes/:id', async (req, res) => {
+  const memes = await Post.findOne({ _id: req.params.id })
+  if(memes === null){
+    res.status(404);
+    return;
+  }
+  console.log(memes)
+  res.status(200).send(memes);
+})
+
+// Posting a meme
+app.post("/memes", async(req, res) => {  
+  try{
+    const name = req.body.name;
+    const url = req.body.url;
+    const caption = req.body.caption;
+
+    const savePost = new Post({
+      name,
+      url,
+      caption
+    })
+    const saved = await savePost.save()
+    res.status(201).send(saved.id)
+  }
+  catch(err){
+    res.status(404).send("error",{error: 'Unable to post'})
+  }
+
+});
+
+// updating a meme
+app.patch('/meme/:id', async (req, res) => {
+  try{
+    const memes = await Post.findById(req.params.id)
+    if(memes === null){
+      res.status(404)
+      return
+    }
+    memes.caption = req.body.caption;
+    memes.url = req.body.url;
+
+    await memes.save();
+    res.status(201)
+    return;
+  }
+  catch (err){
+    res.status(404).send("error",{error: 'Unable to post'})
+  }
+})
+
+// Server listening
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('Server is up mate ' + port)
