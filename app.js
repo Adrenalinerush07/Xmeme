8bbd9ec6b2d23f46b4d1779301f53c7caf70f6e3
cyberghost002 <49005095+cyberghost002@users.noreply.github.com>
2021-02-14 01:38:49 +0530

    cloud and duplicate post

--- app.js
+++ app.js
@@ -13,9 +13,15 @@ app.use(methodOverride('_method'))
 app.use(express.json())
 
 // Connecting to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/Xmeme', {
+// mongoose.connect('mongodb://127.0.0.1:27017/Xmeme', {
+//   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+// })
+
+
+mongoose.connect('mongodb+srv://KartikJaiswal:l13yFSWpb2YU1qR5@blog.smyeg.mongodb.net/Xmemev2?retryWrites=true&w=majority', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
+
 
 // Route for home page (posting a new meme)
 app.get("/", function(req, res){
